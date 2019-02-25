c6216b29f2149ea859f243cd3c12e12a5b3b76d9
ascottb <brownaus@onid.oregonstate.edu>
2019-02-24 17:06:46 -0800

    New pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRouters} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent
   ],
   imports: [
+    AppRouters,
     BrowserModule,
     BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  exports: [
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
